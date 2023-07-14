bbaadebf066ac6e1a0f959e953db4c4e7f7e7a29
asemaydin <muhammetasemaydin@hotmail.com>
2023-07-14 16:52:57 +0300

    header.php oluşturulması

---
+++ HASTANE_OTOMASYON/header.php
@@ -0,0 +1,30 @@
+<!-- php taglarının açılması  -->
+<?php
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hastane Otomasyon</title>
+</head>
+<body>
+
+    <div class="ust_bar">
+        <!-- anasayfa.php ye yönlendiren kod parçası  -->
+        <a href="anasayfa.php"><h1>Hastane Otomasyon</h1></a>
+        <div class="menu">
+            <a href="hesap.php"><h5>Hesap Bilgileri</h5></a>
+            <a href="randevu.php"><h5>Randevu Bilgileri</h5></a>
+        </div>
+    </div>
+    <div class="cikis">
+        Çıkış Yap
+    </div>
+</body>
+</html>
