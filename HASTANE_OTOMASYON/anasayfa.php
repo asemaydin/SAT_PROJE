36af90faa19a348cab085bd782cf7d462b455698
asemaydin <muhammetasemaydin@hotmail.com>
2023-07-14 17:49:45 +0300

    merkez divlerin oluşturulması ve stil.css ekleme

--- HASTANE_OTOMASYON/anasayfa.php
+++ HASTANE_OTOMASYON/anasayfa.php
@@ -1,5 +1,28 @@
-<?php include 'header.php';
+<?php include 'header.php';?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stil.css">
+    <title>Hastane Otomasyonu</title>
+</head>
+<body>
 
+<div class="adsoyad">
+    <h4>Sn. Ad Soyad</h4>
+</div>
+    
+<div class="orta_div" id="randevu_div">
+    
+    </div>
 
-?>
+<div class="orta_div" id="ailehekimi_div" >
+    <h3>Aile Hekimi</h3>
+    <p>
+        Aile Hekiminize Ait Bir Çalışma Saati Bulunmamıştır.
+    </p>
+</div>
+</body>
+</html>
