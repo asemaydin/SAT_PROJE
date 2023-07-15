60cfdfe266141585abdc80096608a4f7d99b3cbd
asemaydin <muhammetasemaydin@hotmail.com>
2023-07-15 13:39:49 +0300

    randevu pph oluşturulması ve stil eklenmesi

--- HASTANE_OTOMASYON/randevu.php
+++ HASTANE_OTOMASYON/randevu.php
@@ -1,2 +1,34 @@
- -->
+<!-- randevu php ile header.php birleştiren kod parçası -->
 <?php include 'header.php' ?>
+
+<!-- temel html kodları -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hastane Otomasyon</title>
+</head>
+<body>
+<!-- Tabloların tasarımını yapabilmek için geçici olarak bu kodlar eklendi tam yerini görmek için aslında bunlar veritabanından çekilecek  -->
+<table>
+    <tr>
+        <th>Hastane</th>
+        <th>Klinik</th>
+        <th>Doktor</th>
+        <th>İl</th>
+        <th>Tarih</th>
+    </tr>
+    <tr>
+        <td>Muhammet Asem Aydin</td>
+        <td>Muhammet Asem Aydin</td>
+        <td>Muhammet Asem Aydin</td>
+        <td>Muhammet Asem Aydin</td>
+        <td>Muhammet Asem Aydin</td>
+    </tr>
+</table>
+    
+</body>
+</html>
