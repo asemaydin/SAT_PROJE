0ec1c67cc79dbb4e4a75edd2736d9484b1462f9e
asemaydin <muhammetasemaydin@hotmail.com>
2023-07-15 17:02:10 +0300

    randevu bilgilerinin veritabanından çekilmesi

--- HASTANE_OTOMASYON/randevu.php
+++ HASTANE_OTOMASYON/randevu.php
@@ -21,13 +21,28 @@
         <th>İl</th>
         <th>Tarih</th>
     </tr>
+<!-- geçici olarak verilen kodların veritabanından çekilmesi için gerken bağlantı kodları -->
+    <?php
+    
+    $randevu_sor = $db->prepare("SELECT * FROM randevu
+    INNER JOIN kullanici ON randevu.randevu_hasta_id = kullanici.kullanici_id WHERE kullanici_tc=:kullanici_tc");
+    $randevu_sor->execute([
+        'kullanici_tc' => $_SESSION['userkullanici_tc']
+    ]);
+    while($randevu_cek = $randevu_sor->fetch(PDO::FETCH_ASSOC)){  ?>
+
+    
+    
+   
+
     <tr>
-        <td>Muhammet Asem Aydin</td>
-        <td>Muhammet Asem Aydin</td>
-        <td>Muhammet Asem Aydin</td>
-        <td>Muhammet Asem Aydin</td>
-        <td>Muhammet Asem Aydin</td>
+        <td><?php echo $randevu_cek['randevu_hastane'];?></td>
+        <td><?php echo $randevu_cek['randevu_klinik'];?></td>
+        <td><?php echo $randevu_cek['randevu_doktor'];?></td>
+        <td><?php echo $randevu_cek['randevu_sehir'];?></td>
+        <td><?php echo $randevu_cek['randevu_tarih'];?></td>
     </tr>
+<?php } ?>
 </table>
     
 </body>
